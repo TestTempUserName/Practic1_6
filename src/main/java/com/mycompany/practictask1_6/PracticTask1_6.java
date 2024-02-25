764159007ba20e30a6fdc44d3f41d4cdb090754f
User <User@HOME-PC.localdomain>
2024-02-25 13:31:02 +0300

    Мой первый коммит!

---
+++ src/main/java/com/mycompany/practictask1_6/PracticTask1_6.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practictask1_6;
+
+/**
+ *
+ * @author User
+ */
+public class PracticTask1_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Practic task code");
+        // какой-то код
+        // выполненного задания
+    }
+}
